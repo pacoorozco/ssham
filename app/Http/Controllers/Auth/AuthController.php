71e7ca491b7fa4b3f04d9f08faa1bcec6054e889
Paco Orozco <paco@pacoorozco.info>
2015-06-20 13:06:05 +0200

    Solves Login on L5.1

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,29 +2,17 @@
 
 namespace SSHAM\Http\Controllers\Auth;
 
+use Auth;
 use SSHAM\User;
 use SSHAM\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use SSHAM\Http\Requests\Auth\LoginRequest;
 
 class AuthController extends Controller
 {
-    /*
-      |--------------------------------------------------------------------------
-      | Registration & Login Controller
-      |--------------------------------------------------------------------------
-      |
-      | This controller handles the registration of new users, as well as the
-      | authentication of existing users. By default, this controller uses
-      | a simple trait to add these behaviors. Why don't you explore it?
-      |
-     */
-
-use AuthenticatesAndRegistersUsers;
 
     /**
      * Create a new authentication controller instance.
      *
-     * @param  Authenticator  $auth
      * @return void
      */
     public function __construct()
@@ -48,18 +36,38 @@ use AuthenticatesAndRegistersUsers;
     }
 
     /**
-     * Create a new user instance after a valid registration.
+     * Show the application login form.
      *
-     * @param  array  $data
-     * @return User
+     * @return Response
      */
-    protected function create(array $data)
+    public function getLogin()
     {
-        return User::create([
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => bcrypt($data['password']),
+        return view('auth.login');
+    }
+
+    public function postLogin(LoginRequest $request)
+    {
+        if (Auth::attempt(['name' => $request->name, 'password' => $request->password, 'active' => 1], $request->has('remember'))) {
+            return redirect()->intended('home');
+        }
+
+        return redirect($this->loginPath())
+                ->withInput($request->only('name', 'remember'))
+                ->withErrors([
+                    'name' => 'These credentials do not match our records. Try again?',
         ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return Response
+     */
+    public function getLogout()
+    {
+        Auth::logout();
+
+        return redirect('/');
     }
 
 }
---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SSHAM\Http\Requests\Auth;
+
+use SSHAM\Http\Requests\Request;
+
+class LoginRequest extends Request
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'password' => 'required',
+        ];
+    }
+
+}
