65e5dda1e0ded612c587368c67c4f77a07329634
Paco Orozco <paco@pacoorozco.info>
2015-08-08 08:07:20 +0200

    doc: Add documentation
    Signed-off-by: Paco Orozco <paco@pacoorozco.info>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,16 +20,14 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define your route model bindings, pattern filters, etc.
      *
-     * @param  \Illuminate\Routing\Router  $router
+     * @param  \Illuminate\Routing\Router $router
      * @return void
      */
     public function boot(Router $router)
     {
-        //
-
         parent::boot($router);
 
-        # Define route model bindings
+        // Define route model bindings
         $router->model('users', 'SSHAM\User');
         $router->model('hosts', 'SSHAM\Host');
         $router->model('usergroups', 'SSHAM\Usergroup');
@@ -41,12 +39,12 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define the routes for the application.
      *
-     * @param  \Illuminate\Routing\Router  $router
+     * @param  \Illuminate\Routing\Router $router
      * @return void
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function($router) {
+        $router->group(['namespace' => $this->namespace], function ($router) {
             require app_path('Http/routes.php');
         });
     }
