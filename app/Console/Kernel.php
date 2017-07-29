69e114fa3e6681fb0d064aecbcc9e7570d857677
Paco Orozco <paco@pacoorozco.info>
2017-07-29 16:18:00 +0200

    Change licensing (#9)
    * feat: Change licensing to GPLv3
    
    Prior license was MIT, but I think GPLv3 is a best license for this
    work.
    
    Signed-off-by: Paco Orozco <paco@pacoorozco.info>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,4 +1,21 @@
-<?php namespace SSHAM\Console;
+<?php
+/**
+ * SSHAM - SSH Access Manager Web Interface.
+ *
+ * Copyright (c) 2017 by Paco Orozco <paco@pacoorozco.info>
+ *
+ * This file is part of some open source application.
+ *
+ * Licensed under GNU General Public License 3.0.
+ * Some rights reserved. See LICENSE, AUTHORS.
+ *
+ * @author      Paco Orozco <paco@pacoorozco.info>
+ * @copyright   2017 Paco Orozco
+ * @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ * @link        https://github.com/pacoorozco/ssham
+ */
+
+namespace SSHAM\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
