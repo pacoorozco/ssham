5c4de685623a1ca369cf5d6d8646118e21628c81
pacoorozco <paco@pacoorozco.info>
2019-12-07 08:07:59 +0100

    Update copyright notice
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -2,15 +2,15 @@
 /**
  * SSH Access Manager - SSH keys management solution.
  *
- * Copyright (c) 2019 by Paco Orozco <paco@pacoorozco.info>
+ * Copyright (c) 2017 - 2019 by Paco Orozco <paco@pacoorozco.info>
  *
  *  This file is part of some open source application.
- *  
+ *
  *  Licensed under GNU General Public License 3.0.
  *  Some rights reserved. See LICENSE, AUTHORS.
- *  
+ *
  *  @author      Paco Orozco <paco@pacoorozco.info>
- *  @copyright   2019 Paco Orozco
+ *  @copyright   2017 - 2019 Paco Orozco
  *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
  *  @link        https://github.com/pacoorozco/ssham
  */
--- public/index.php
+++ public/index.php
@@ -1,10 +1,18 @@
 <?php
-
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * SSH Access Manager - SSH keys management solution.
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * Copyright (c) 2017 - 2019 by Paco Orozco <paco@pacoorozco.info>
+ *
+ *  This file is part of some open source application.
+ *
+ *  Licensed under GNU General Public License 3.0.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @author      Paco Orozco <paco@pacoorozco.info>
+ *  @copyright   2017 - 2019 Paco Orozco
+ *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *  @link        https://github.com/pacoorozco/ssham
  */
 
 define('LARAVEL_START', microtime(true));
