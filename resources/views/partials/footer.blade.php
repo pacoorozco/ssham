a2d29edcd13ed28fa762ee8a7336aef8203aa40f
pacoorozco <paco@pacoorozco.info>
2019-12-20 18:56:28 +0100

    Fix copyright date
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2016 <a href="http://pacoorozco.info" rel="nofollow">Paco Orozco</a></strong>
+    <strong>Copyright &copy; 2013-{{ date('Y') }} <a href="http://pacoorozco.info" rel="nofollow">Paco Orozco</a></strong>
     <div class="float-right d-none d-sm-inline-block">
         Powered
         by <a href="https://github.com/pacoorozco/ssham" rel="nofollow">SSH Access Manager</a>
