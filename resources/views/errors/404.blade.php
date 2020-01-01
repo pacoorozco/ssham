d3c332139f6974572f9befecd06e8c82bb5aef2a
pacoorozco <paco@pacoorozco.info>
2020-01-01 19:31:01 +0100

    Add tests to LoginController
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.error')
 
+{{-- Title --}}
+@section('title', 'Error 404')
+
+@section('content')
+    <h1 class="error">404 Error</h1>
+    <h4>Oops! This page Could Not Be Found!</h4>
+    <p>Sorry bit the page you are looking for does not exist, have been removed or name changed.</p>
+    <a href="{{ route('home') }}" class="btn btn-primary">Go to homepage</a>
+@endsection
+
 
---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    /** @test */
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+}
