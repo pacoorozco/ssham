a4416e733425849f90686a98b12b7854a8930cab
Paco Orozco <pacoorozco@users.noreply.github.com>
2020-10-15 15:51:28 +0000

    Apply fixes from StyleCI

--- app/Host.php
+++ app/Host.php
@@ -42,12 +42,12 @@ class Host extends Model implements Searchable
     /**
      * Host statuses.
      */
-    const INITIAL_STATUS         = 'INITIAL';
-    const AUTH_FAIL_STATUS       = 'AUTHFAIL';
+    const INITIAL_STATUS = 'INITIAL';
+    const AUTH_FAIL_STATUS = 'AUTHFAIL';
     const PUBLIC_KEY_FAIL_STATUS = 'KEYAUTHFAIL';
-    const GENERIC_FAIL_STATUS    = 'GENERICFAIL';
-    const SUCCESS_STATUS         = 'SUCCESS';
-    const HOST_FAIL_STATUS       = 'HOSTFAIL';
+    const GENERIC_FAIL_STATUS = 'GENERICFAIL';
+    const SUCCESS_STATUS = 'SUCCESS';
+    const HOST_FAIL_STATUS = 'HOSTFAIL';
 
     /**
      * The database table used by the model.
@@ -135,7 +135,7 @@ class Host extends Model implements Searchable
      */
     public function getFullHostnameAttribute()
     {
-        return $this->username . '@' . $this->hostname . ':' . $this->port;
+        return $this->username.'@'.$this->hostname.':'.$this->port;
     }
 
     /**
@@ -150,7 +150,7 @@ class Host extends Model implements Searchable
     {
         $this->synced = $synced;
 
-        if (!$skip_save) {
+        if (! $skip_save) {
             $this->save();
         }
     }
@@ -202,7 +202,7 @@ class Host extends Model implements Searchable
                             break;
                         case 'allow':
                             $content = explode(' ', $key->public, 3);
-                            $content[2] = $key->username . '@ssham';
+                            $content[2] = $key->username.'@ssham';
                             $sshKeys[$key->username] = join(' ', $content);
                             break;
                         default:
@@ -211,7 +211,7 @@ class Host extends Model implements Searchable
                 }
             }
         }
-        if (!is_null($bastionSSHPublicKey)) {
+        if (! is_null($bastionSSHPublicKey)) {
             $sshKeys[] = $bastionSSHPublicKey;
         }
 
