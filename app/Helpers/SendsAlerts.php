b56c1e44fb10fd0c74b9e8d29dc69d24bc6ae7c5
pacoorozco <paco@pacoorozco.info>
2021-05-04 14:43:40 +0200

    Add a helper to send alerts to web UI
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ app/Helpers/SendsAlerts.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * SSH Access Manager - SSH keys management solution.
+ *
+ * Copyright (c) 2017 - 2021 by Paco Orozco <paco@pacoorozco.info>
+ *
+ *  This file is part of some open source application.
+ *
+ *  Licensed under GNU General Public License 3.0.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @author      Paco Orozco <paco@pacoorozco.info>
+ *  @copyright   2017 - 2021 Paco Orozco
+ *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *  @link        https://github.com/pacoorozco/ssham
+ */
+
+namespace App\Helpers;
+
+trait SendsAlerts
+{
+    protected function success(string $id = null, $parameters = []): void
+    {
+        $this->sendAlert('success', $id, $parameters);
+    }
+
+    protected function error(string $id = null, $parameters = []): void
+    {
+        $this->sendAlert('error', $id, $parameters);
+    }
+
+    private function sendAlert(string $type, string $id = null, $parameters = []): void
+    {
+        session([$type => __($id, (array) $parameters)]);
+    }
+}
