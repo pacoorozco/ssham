e7448c027303ac43aa7b45ebd266a0cc0989e666
pacoorozco <paco@pacoorozco.info>
2021-05-13 09:45:54 +0200

    Fix previos commit: forgot to add Listeners
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ app/Listeners/RemovePrivateKey.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * SSH Access Manager - SSH keys management solution.
+ *
+ * Copyright (c) 2017 - 2021 by Paco Orozco <paco@pacoorozco.info>
+ *
+ *  This file is part of some open source application.
+ *
+ *  Licensed under GNU General Public License 3.0.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @author      Paco Orozco <paco@pacoorozco.info>
+ *  @copyright   2017 - 2021 Paco Orozco
+ *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *  @link        https://github.com/pacoorozco/ssham
+ */
+
+namespace App\Listeners;
+
+use App\Events\PrivateKeyWasDownloaded;
+use App\Models\Activity;
+
+class RemovePrivateKey
+{
+    public function handle(PrivateKeyWasDownloaded $event): void
+    {
+        $key = $event->getKey();
+        $key->save([
+            'private' => null,
+        ]);
+
+        activity()
+            ->performedOn($key)
+            ->withProperties(['status' => Activity::STATUS_SUCCESS])
+            ->log(sprintf("Private key was removed from ky '%s'.", $key->username));
+    }
+}
