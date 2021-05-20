52346c6656d16eae74659e53fa89be494109aae9
pacoorozco <paco@pacoorozco.info>
2021-05-20 12:01:51 +0200

    Move Models unit tests to its own folder
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -1,6 +1,21 @@
 <?php
+/*
+ * SSH Access Manager - SSH keys management solution.
+ *
+ * Copyright (c) 2017 - 2021 by Paco Orozco <paco@pacoorozco.info>
+ *
+ *  This file is part of some open source application.
+ *
+ *  Licensed under GNU General Public License 3.0.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @author      Paco Orozco <paco@pacoorozco.info>
+ *  @copyright   2017 - 2021 Paco Orozco
+ *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *  @link        https://github.com/pacoorozco/ssham
+ */
 
-namespace Tests\Unit;
+namespace Tests\Unit\Models;
 
 use App\Models\User;
 use Tests\ModelTestCase;
