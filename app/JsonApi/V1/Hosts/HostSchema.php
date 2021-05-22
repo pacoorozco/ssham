a0881b2aed808aba06ce3a33fb42965dd3637a6c
Paco Orozco <paco@pacoorozco.info>
2021-05-22 04:30:24 +0200

    Apply fixes from StyleCI (#143)

--- app/JsonApi/V1/Hosts/HostSchema.php
+++ app/JsonApi/V1/Hosts/HostSchema.php
@@ -41,7 +41,7 @@ class HostSchema extends Schema
             Boolean::make('synced')->readOnly(),
             Str::make('syncedStatus', 'status_code')
                 ->serializeUsing(
-                    static fn($value) => $value->description
+                    static fn ($value) => $value->description
                 )
                 ->readOnly(),
             DateTime::make('syncedAt', 'last_rotation')->readOnly(),
