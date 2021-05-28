63c0e620172718b8a045c56bb9ddffdffc7905c0
Paco Orozco <pacoorozco@users.noreply.github.com>
2021-05-28 12:34:29 +0000

    Apply fixes from StyleCI

--- resources/lang/en/hostgroup/messages.php
+++ resources/lang/en/hostgroup/messages.php
@@ -30,5 +30,4 @@ return [
     'delete_confirmation_warning' => 'This action <strong>cannot</strong> be undone. This will permanently delete the <strong>:name</strong> group and remove all related rules.',
     'delete_confirmation_button' => 'I understand the consequences, delete this group',
 
-
 ];
