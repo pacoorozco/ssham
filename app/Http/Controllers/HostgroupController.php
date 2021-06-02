701f3a150e40ecd4054a244ae6c9f073b5993724
pacoorozco <paco@pacoorozco.info>
2021-06-02 11:48:41 +0200

    Implement Hostgroup controller w/ jobs
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Http/Controllers/HostgroupController.php
+++ app/Http/Controllers/HostgroupController.php
@@ -19,6 +19,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HostgroupCreateRequest;
 use App\Http\Requests\HostgroupUpdateRequest;
+use App\Jobs\CreateHostgroup;
+use App\Jobs\DeleteHostgroup;
+use App\Jobs\UpdateHostgroup;
 use App\Models\Host;
 use App\Models\Hostgroup;
 use Illuminate\Http\JsonResponse;
@@ -44,21 +47,11 @@ class HostgroupController extends Controller
 
     public function store(HostgroupCreateRequest $request): RedirectResponse
     {
-        try {
-            $hostgroup = Hostgroup::create([
-                'name' => $request->name,
-                'description' => $request->description,
-            ]);
-
-            // Associate Host's Groups
-            if ($request->filled('hosts')) {
-                $hostgroup->hosts()->sync($request->hosts);
-            }
-        } catch (\Exception $e) {
-            return redirect()->back()
-                ->withInput()
-                ->withErrors(__('hostgroup/messages.create.error'));
-        }
+        $hostgroup = CreateHostgroup::dispatchSync(
+            $request->name(),
+            $request->description(),
+            $request->hosts()
+        );
 
         return redirect()->route('hostgroups.index')
             ->withSuccess(__('hostgroup/messages.create.success', ['name' => $hostgroup->name]));
@@ -82,47 +75,23 @@ class HostgroupController extends Controller
 
     public function update(Hostgroup $hostgroup, HostgroupUpdateRequest $request): RedirectResponse
     {
-        try {
-            $hostgroup->update([
-                'name' => $request->name,
-                'description' => $request->description,
-            ]);
+        UpdateHostgroup::dispatchSync(
+            $hostgroup,
+            $request->name(),
+            $request->description(),
+            $request->hosts()
+        );
 
-            // Associate User's Groups
-            if ($request->filled('hosts')) {
-                $hostgroup->hosts()->sync($request->hosts);
-            } else {
-                $hostgroup->hosts()->detach();
-            }
-        } catch (\Exception $exception) {
-            return redirect()->back()
-                ->withInput()
-                ->withErrors(__('hostgroup/messages.edit.error'));
-        }
-
-        return redirect()->route('hostgroups.edit', [$hostgroup->id])
+        return redirect()->route('hostgroups.edit', [$hostgroup])
             ->withSuccess(__('hostgroup/messages.edit.success', ['name' => $hostgroup->name]));
-    }
-
-    public function delete(Hostgroup $hostgroup): View
-    {
-        return view('hostgroup.delete')
-            ->with('hostgroup', $hostgroup);
     }
 
     public function destroy(Hostgroup $hostgroup): RedirectResponse
     {
-        $name = $hostgroup->name;
-
-        try {
-            $hostgroup->delete();
-        } catch (\Exception $exception) {
-            return redirect()->back()
-                ->withErrors(__('hostgroup/messages.delete.error'));
-        }
+        DeleteHostgroup::dispatchSync($hostgroup);
 
         return redirect()->route('hostgroups.index')
-            ->withSuccess(__('hostgroup/messages.delete.success', ['name' => $name]));
+            ->withSuccess(__('hostgroup/messages.delete.success', ['name' => $hostgroup->name]));
     }
 
     public function data(Datatables $datatable): JsonResponse
