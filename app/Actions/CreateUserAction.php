708aeb76a8e320068c98709ed1788d634918f394
pacoorozco <paco@pacoorozco.info>
2022-03-22 08:54:23 +0100

    Refactor CreateUserAction
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ app/Actions/CreateUserAction.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * SSH Access Manager - SSH keys management solution.
+ *
+ * Copyright (c) 2017 - 2022 by Paco Orozco <paco@pacoorozco.info>
+ *
+ *  This file is part of some open source application.
+ *
+ *  Licensed under GNU General Public License 3.0.
+ *  Some rights reserved. See LICENSE, AUTHORS.
+ *
+ *  @author      Paco Orozco <paco@pacoorozco.info>
+ *  @copyright   2017 - 2022 Paco Orozco
+ *  @license     GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *  @link        https://github.com/pacoorozco/ssham
+ */
+
+namespace App\Actions;
+
+use App\Enums\Roles;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserAction
+{
+    public function __invoke(
+        string $username,
+        string $email,
+        string $password,
+        ?string $role = null,
+    ): User {
+        /* @var User $user */
+        $user = User::create([
+            'username' => $username,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $role = Roles::coerce($role)?->value ?? Roles::Auditor;
+
+        return $user->assignRole($role);
+    }
+}
