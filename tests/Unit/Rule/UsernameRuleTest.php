08f3c0c2605589f72984e2bf5d428a1e8cbbcb77
Paco Orozco <paco@pacoorozco.info>
2022-04-26 18:05:50 +0200

    Apply fixes from StyleCI (#289)

--- tests/Unit/Rule/UsernameRuleTest.php
+++ tests/Unit/Rule/UsernameRuleTest.php
@@ -49,7 +49,6 @@ class UsernameRuleTest extends TestCase
         $rule = new UsernameRule();
 
         $this->assertFalse($rule->passes('username', $input));
-
     }
 
     public function provideInvalidUsernames(): Generator
