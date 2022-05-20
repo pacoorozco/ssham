9d46549fdd54f9f905c60e428fa326dc15b6d6c0
pacoorozco <paco@pacoorozco.info>
2022-05-20 09:01:17 +0200

    Add logs to the UpdateServer job
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Jobs/UpdateServer.php
+++ app/Jobs/UpdateServer.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use PacoOrozco\OpenSSH\PrivateKey;
 
@@ -26,17 +27,17 @@ class UpdateServer implements ShouldQueue
     public function __construct(Host $host)
     {
         $this->host = $host;
-
-        $this->pusher = new SFTPPusher(
-            $this->host->hostname,
-            $this->host->portOrDefaultSetting(),
-            setting()->get('ssh_timeout'),
-        );
     }
 
     public function handle(): void
     {
         try {
+            $this->pusher = new SFTPPusher(
+                hostname: $this->host->hostname,
+                port: $this->host->portOrDefaultSetting(),
+                timeout: setting()->get('ssh_timeout'),
+            );
+
             $this->connectRemoteServer();
 
             $this->sendRemoteUpdaterCLI();
@@ -45,8 +46,11 @@ class UpdateServer implements ShouldQueue
 
             $this->execRemoteUpdater();
         } catch (\Throwable $exception) {
+            Log::error("Updating server '" . $this->host->full_hostname . "' got this error: " . $exception->getMessage());
+
             $this->host->setStatus(HostStatus::GENERIC_FAIL_STATUS());
             $this->fail($exception);
+            return;
         }
 
         $this->host->setStatus(HostStatus::SUCCESS_STATUS());
@@ -55,23 +59,28 @@ class UpdateServer implements ShouldQueue
     }
 
     /**
-     * @throws \App\Exceptions\PusherException
+     * @throws \App\Exceptions\PusherException|\PacoOrozco\OpenSSH\Exceptions\InvalidPrivateKey
      */
     protected function connectRemoteServer(): void
     {
-        $privateKey = PrivateKey::fromString(setting()->get('private_key'));
-        $this->pusher->login($this->host->username, $privateKey);
+        $this->pusher->login(
+            username: $this->host->username,
+            privateKey: PrivateKey::fromString(setting()->get('private_key'))
+        );
     }
 
     /**
      * @throws \App\Exceptions\PusherException
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     protected function sendRemoteUpdaterCLI(): void
     {
         $remoteUpdater = Storage::disk('private')->get('ssham-remote-updater.sh');
-        if (! is_null($remoteUpdater)) {
-            $this->pusher->pushFileTo($remoteUpdater, setting()->get('cmd_remote_updater'), 0700);
+        if (!is_null($remoteUpdater)) {
+            $this->pusher->pushFileTo(
+                localPath: $remoteUpdater,
+                remotePath: setting()->get('cmd_remote_updater'),
+                permission: 0700
+            );
         }
     }
 
@@ -81,7 +90,11 @@ class UpdateServer implements ShouldQueue
     protected function updateRemoteSSHKeys(): void
     {
         $sshKeys = $this->host->getSSHKeysForHost(setting()->get('public_key'));
-        $this->pusher->pushDataTo(join(PHP_EOL, $sshKeys), setting()->get('ssham_file'), 0600);
+        $this->pusher->pushDataTo(
+            data: join(PHP_EOL, $sshKeys),
+            remotePath: setting()->get('ssham_file'),
+            permission: 0600
+        );
     }
 
     /**
@@ -89,11 +102,11 @@ class UpdateServer implements ShouldQueue
      */
     protected function execRemoteUpdater(): void
     {
-        $command = setting()->get('cmd_remote_updater').' update '
-            .((setting()->get('mixed_mode') == '1') ? 'true ' : 'false ')
-            .setting()->get('authorized_keys').' '
-            .setting()->get('non_ssham_file').' '
-            .setting()->get('ssham_file');
+        $command = setting()->get('cmd_remote_updater') . ' update '
+            . ((setting()->get('mixed_mode') == '1') ? 'true ' : 'false ')
+            . setting()->get('authorized_keys') . ' '
+            . setting()->get('non_ssham_file') . ' '
+            . setting()->get('ssham_file');
 
         $this->pusher->exec($command);
     }
