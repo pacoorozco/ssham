11a5ae64e41010321868d6ddf490afd3c62323dc
pacoorozco <paco@pacoorozco.info>
2022-11-12 17:09:32 +0100

    Fix factories using colliding colorNames
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/factories/HostgroupFactory.php
+++ database/factories/HostgroupFactory.php
@@ -31,7 +31,7 @@ class HostgroupFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'Group_'.$this->faker->unique()->colorName,
+            'name' => 'Group_'.$this->faker->unique()->word(),
             'description' => $this->faker->text,
         ];
     }
